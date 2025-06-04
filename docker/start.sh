61d428d36752be1f7ff25284291918bfedbb90ae
Aigentis <admin@aigentis.io>
2025-06-04 01:04:38 +0000

    Update start.sh

--- docker/start.sh
+++ docker/start.sh
@@ -1,7 +1,14 @@
-#!/bin/bash
+#!/bin/sh
 
 # Start Nginx in the background
-nginx -g "daemon off;" &
+nginx -g 'daemon off;' &
 
-# Start the langflow application
-langflow run
+# Start Langflow in the foreground
+# Ensure all necessary LANGFLOW environment variables are set in Dokploy or the Dockerfile
+# (LANGFLOW_DATABASE_URL, LANGFLOW_SECRET_KEY, LANGFLOW_AUTO_LOGIN, etc.)
+echo "Starting Langflow application..."
+langflow run --host "$LANGFLOW_HOST" --port "$LANGFLOW_PORT"
+
+# If langflow run daemonizes itself or you want nginx in foreground instead:
+# langflow run --host "$LANGFLOW_HOST" --port "$LANGFLOW_PORT" &
+# nginx -g 'daemon off;'
